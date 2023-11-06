9043b5268aa8eebec221256aad6872f6a5157430
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-06 18:33:20 +0100

    hello2.c

---
+++ Exercise_3/hello2.c
@@ -0,0 +1,16 @@
+/*---- Aufgabe 10 b). -----*/
+#include <stdio.h>
+
+int main()
+{
+    printf("/* Das ultimative Hello-World-Programm */\n");
+    printf("#include <stdio.h>\n");
+   
+    printf("int main(void)\n");
+    printf("{\n");
+    
+    printf(" printf(\"Hello Welt!\\n\");\n");
+    printf("}\n");
+
+    return 0;
+}
