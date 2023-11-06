bfd030c27e50fd6fa2085822465421334155ade4
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-06 18:37:18 +0100

    hello3.c

---
+++ Exercise_3/hello3.c
@@ -0,0 +1,13 @@
+/*---- Aufgabe 10 c). -----*/
+#include <stdio.h>
+
+int main()
+{
+    char s[100];
+  
+    printf("Enter your name: ");
+    scanf("%99s",s);
+    printf("Hello, %s\n!",s);
+  
+    return 0;
+}
