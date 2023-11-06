1fc3eb1a764c06d31ed970bcf2a1d3f807f0a0cd
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-06 19:08:28 +0100

    arithmetic.c

---
+++ Exercise_3/arithmetic.c
@@ -0,0 +1,15 @@
+/*---- Aufgabe 12 -----*/
+#include <stdio.h>
+
+int main()
+{
+double a=2.0, b=3.0, c=0.4E1, p=3.14;
+
+printf("Aufgabe 12 a). Ergebnis ist %lf\n",5*a+76+3.1);
+printf("Aufgabe 12 b). Ergebnis ist %lf\n",(a+6)/(2*p*a+(1/a)));
+printf("Aufgabe 12 c). Ergebnis ist %lf\n",(a+6)/((b-(p/c)/a+(c/a-(1/b)))));
+printf("Aufgabe 12 d). Ergebnis ist %lf\n",1/(a*a));
+printf("Aufgabe 12 e). Ergebnis ist %lf\n",-a*-a);
+
+ return 0;
+}
