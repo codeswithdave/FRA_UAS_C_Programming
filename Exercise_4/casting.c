b8ec376785e88b89cb982f61ffcdac417eedd2b2
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-10 11:30:28 +0100

    casting.c

---
+++ Exercise_4/casting.c
@@ -0,0 +1,16 @@
+/*---- Aufgabe 16 -----*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Aufgabenteil a). \n");
+    printf("Die Rechnung 12 / 8 / 2 ergibt: %d\n",12/8/2);
+    printf("Die Rechnung 5 / 50 * 10 ergibt: %d\n",5/50*10);
+    printf("Die Rechnung 3 / 6 / 9 ergibt: %d\n\n",3/6/9);
+
+    printf("Aufgabenteil b).  \n");
+    printf("Die Rechnung 12 / 8 / 2 ergibt: %f\n",((float)12/(float)8/(float)2));
+    printf("Die Rechnung 5 / 50 * 10 ergibt: %f\n",((float)5/(float)50*(float)10));
+    printf("Die Rechnung 3 / 6 / 9 ergibt: %f\n",((float)3/(float)6/(float)9));
+ return 0;
+}
