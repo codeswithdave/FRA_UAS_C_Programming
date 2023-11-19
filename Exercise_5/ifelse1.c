59f7231d0d8f70781c70997f28c97a09d4c26358
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-19 14:27:10 +0100

    ifelse1.c

---
+++ Exercise_5/ifelse1.c
@@ -0,0 +1,35 @@
+/*---- Aufgabe 19 -----*/
+#include <stdio.h>
+
+int main(){
+
+    int i,k, m=1;
+    char a;
+    k=2;
+    a="0";
+
+    //If (k=1+2 && i=0) = ergibt False.
+    if((k=m+2)&&(i=a))
+
+    //If(0>1) = ergibt False Anwesiung wird übersprungen.
+    if(a>'1'){k=4;a='1';}
+
+    // If (k==3 && 2024) = Ergibt true.
+    else if ((k==3)&&1024)
+
+    //If(K==3) = ergibt true und i=-6
+    {if(k==3); i-=6}
+
+    //if(m=42) = egibt true und k=7.
+    if(m=42) k=7;
+
+    /*Am Ende ist:
+     i=-6
+     k=7
+
+     m=42
+     a='0'
+
+     */
+    return 0;
+}
