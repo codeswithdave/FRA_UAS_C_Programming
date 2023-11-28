28b73d598f060da28ad0f4779247fdb0fc54d1a8
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-28 11:01:32 +0100

    expreissionandassignment.c

---
+++ Lecture_1/expreissionandassignment.c
@@ -0,0 +1,13 @@
+#include <stido.h>
+
+int main(){
+//Erstellung der Variablen  
+int i,k; 
+  
+  i+2*k-10; //Ausdrucksanwesiung
+  i=5;//Zuweisung und Anweisung 
+  k=i+2;//Zuwisung und Anweisung
+  printf("Hello!\n"); //Anweisung
+  
+  return 0; //Anweisung
+}
