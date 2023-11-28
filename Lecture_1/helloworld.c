49296e4a5a958db88fd72605a8955fc4b8814654
codeswithdave <110048221+codeswithdave@users.noreply.github.com>
2023-11-28 09:51:47 +0100

    helloworld.c

--- Lecture_1/helloworld.c
+++ Lecture_1/helloworld.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-int main(){
-   printf("Hello World!\n"); 
-
-  return 0; 
+#include <stdio.h> //Includier die Standart Biiblothek für ein und Ausgabe
+int main(){ //Hauptfunktion eines C Programms
+   printf("Hello World!\n"); //Funktion printf aus der Standartbibolthek stdio.h
+  return 0; //Rückgabewert 0 
 }
+
+//Kompilieren eine Programms unter Linux  gcc hello.c -o hello 
+//Ausführen eines C Programms unter Linux ./hello
